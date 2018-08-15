d22ac2c4c4b27a906e544703166e1e5143b29f19
Luã de Souza <lua.desouza@theiconic.com.au>
2018-08-15 15:27:24 +1000

    Use a Guzzle client instead of cURL

--- src/Client/NtlmSoap.php
+++ src/Client/NtlmSoap.php
@@ -2,17 +2,26 @@
 
 namespace TheIconic\NtlmSoap\Client;
 
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\RequestException;
 use SoapClient;
 use SoapFault;
 
 class NtlmSoap extends SoapClient
 {
-    protected $user;
+    protected $client;
+    protected $username;
     protected $password;
 
-    public function __construct(string $user, string $password, string $wsdl, array $options = [])
-    {
-        $this->user = $user;
+    public function __construct(
+        ClientInterface $client,
+        string $username,
+        string $password,
+        string $wsdl,
+        array $options = []
+    ) {
+        $this->client = $client;
+        $this->username = $username;
         $this->password = $password;
 
         parent::__construct($wsdl, $options);
@@ -24,31 +33,22 @@ class NtlmSoap extends SoapClient
     public function __doRequest($request, $location, $action, $version, $oneWay = 0): string
     {
         $headers = [
-            'Method: POST',
-            'Connection: Keep-Alive',
-            'User-Agent: PHP-SOAP-CURL',
-            'Content-type: text/xml; charset=utf-8',
-            sprintf('SOAPAction: "%s"', $action),
+            'Connection' => 'Keep-Alive',
+            'Content-type' => 'text/xml; charset=utf-8',
+            'SOAPAction' => sprintf('"%s"', $action),
         ];
 
-        $handle = curl_init($location);
-        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($handle, CURLOPT_POST, true);
-        curl_setopt($handle, CURLOPT_POSTFIELDS, $request);
-        curl_setopt($handle, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-        curl_setopt($handle, CURLOPT_HTTPAUTH, CURLAUTH_NTLM);
-        curl_setopt($handle, CURLOPT_USERPWD, $this->user.':'.$this->password);
-
-        $response = (string)curl_exec($handle);
-
-        if ($error = curl_error($handle)) {
-            throw new SoapFault(
-                'Error accessing WSDL server: '.$error, curl_errno($handle)
-            );
+        try {
+            $response = $this->client->request('POST', $location, [
+                'headers' => $headers,
+                'auth' => [$this->username, $this->password, 'ntlm'],
+                'body' => $request,
+                'http_errors' => false,
+            ]);
+        } catch (RequestException $exception) {
+            throw new SoapFault($exception->getCode(), $exception->getMessage());
         }
-        curl_close($handle);
 
-        return $response;
+            return (string) $response->getBody();
+        }
     }
-}
